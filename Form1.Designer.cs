5e60ec7ec542ec7296c94e1871871833f7fa57ab
PS2-4-3V3R <49728419+cesar-0n@users.noreply.github.com>
2024-08-25 12:52:56 +0200

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace WCSharpExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.devToolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consolaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1101, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.devToolsToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // devToolsToolStripMenuItem
+            // 
+            this.devToolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consolaToolStripMenuItem});
+            this.devToolsToolStripMenuItem.Name = "devToolsToolStripMenuItem";
+            this.devToolsToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.devToolsToolStripMenuItem.Text = "Dev Tools";
+            // 
+            // consolaToolStripMenuItem
+            // 
+            this.consolaToolStripMenuItem.Name = "consolaToolStripMenuItem";
+            this.consolaToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.consolaToolStripMenuItem.Text = "Console";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.listView1.HideSelection = false;
+            this.listView1.LabelEdit = true;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(107, 27);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(982, 535);
+            this.listView1.StateImageList = this.imageList1;
+            this.listView1.TabIndex = 4;
+            this.listView1.TileSize = new System.Drawing.Size(64, 64);
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "imageres_162.ico");
+            this.imageList1.Images.SetKeyName(1, "imageres_19.ico");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 574);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem devToolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consolaToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿#pragma warning disable IDE1006
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Collections;
+
+namespace WCSharpExplorer
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("msvcrt.dll")]
+        public static extern int system(string format);
+        public Form1()
+        {
+            InitializeComponent();
+            AllocConsole();
+            system("");
+        }
+
+        string[] GetDir(string address, string arg)
+        {
+            //Console.WriteLine("====== getFileNames =======");
+            List<string> file_PnE;
+            switch (arg)
+            {
+                case "A":
+                    file_PnE = Directory.GetFiles(address).ToList(); //file_PnE = file path and extension
+                    break;
+                case "D":
+                    file_PnE = Directory.GetDirectories(address).ToList(); //file_PnE = file path and extension
+                    break;
+                default:
+                    Console.WriteLine("[ERROR]: in dir function");
+                    string[] err = { "ERROR" };
+                    return err;
+            }
+            List<string> file_E = new List<string>();
+            foreach (string f in file_PnE)
+            {
+                file_E.Add(f.Split('\\').Last());
+                //System.Console.WriteLine(file_E.Last());
+            }
+            return file_E.ToArray();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> files = new List<string>();
+            List<int> files_type = new List<int>();
+            foreach (string item in GetDir("C:\\PMBM\\C#\\WCSharpExplorer", "D"))
+            {
+                files.Add(item);
+                files_type.Add(0);
+            }
+            foreach (string item in GetDir("C:\\PMBM\\C#\\WCSharpExplorer", "A"))
+            {
+                files.Add(item);
+                files_type.Add(1);
+            }
+            Console.WriteLine("====== OUTPUT =======");
+            for (int i = 0; i < files.Count(); i++)
+            {
+                Console.WriteLine(files[i]);
+                listView1.Items.Add(files[i], files_type[i]);
+                listView1.LargeImageList = listView1.LargeImageList;
+            }
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            string tal = listView1.SelectedItems[0].Text.ToString();
+            Console.WriteLine(tal);
+        }
+    }
+}
